ecbb6d88920c5cda840ce4d0f3cbd272e3891022
Oliver Veits <oliver.veits@gmail.com>
2019-02-08 18:58:18 +0100

    hotfix 1_install_helm.sh

--- 4_create_letsencrypt/1_install_helm.sh
+++ 4_create_letsencrypt/1_install_helm.sh
@@ -4,8 +4,8 @@ helm version
 # install helm, if it is not already installed:
 if [ "$?" != "0" ]; then
   curl -s -o helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.12.1-linux-amd64.tar.gz \
-  && mkdir helm-download
-  && cd helm-download
+  && mkdir helm-download \
+  && cd helm-download \
   && sudo tar -zxvf helm.tar.gz \
   && sudo cp -p -f linux-amd64/helm /usr/local/bin/ \
   && helm init \
