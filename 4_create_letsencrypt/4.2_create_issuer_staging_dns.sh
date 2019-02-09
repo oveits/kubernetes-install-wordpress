7fefafc99a2f0b12aa8d03d7e0d3fbc5b365a809
Oliver Veits <oliver.veits@gmail.com>
2019-02-09 21:41:07 +0100

    removed unneeded zone and added namespace

--- 4_create_letsencrypt/4.2_create_issuer_staging_dns.sh
+++ 4_create_letsencrypt/4.2_create_issuer_staging_dns.sh
@@ -1,6 +1,11 @@
+
+if [ "$NAMESPACE" == "" ]; then
+  echo "usage: export NAMESPACE=staging; bash $0"
+  exit 1
+fi
+
 source $HOME/.cloudflare/credentials.sh || exit "credentials file $HOME/.cloudflare/credentials.sh not found"
 # the file $HOME/.cloudflare/credentials.sh is assumed to have the format
-# ZONE=your_cloudflare_zone
 # EMAIL=your_cloudflare_email_address
 # GLOBAL_API_KEY=your_cloudflare_global_api_key
 
@@ -16,6 +21,7 @@ apiVersion: certmanager.k8s.io/v1alpha1
 kind: Issuer
 metadata:
   name: letsencrypt-staging-dns
+  namespace: ${NAMESPACE}
 spec:
   acme:
     server: https://acme-staging-v02.api.letsencrypt.org/directory
@@ -27,7 +33,6 @@ spec:
         - name: my-cloudflare-provider
           cloudflare:
             email: ${EMAIL}
-            zone: ${ZONE}
             apiKeySecretRef: 
               name: cloudflare-api-key
               key: cloudflare-api-key.txt
