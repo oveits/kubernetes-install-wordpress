a2c1084f4daeae3b737f920f10bea7e7f08da9d7
Oliver Veits <oliver.veits@gmail.com>
2019-04-09 23:17:55 +0200

    2GB file size and 1200 sec upload timeout

---
+++ 4_install_wordpress/6_ingress_allow_large_file_upload.sh
@@ -0,0 +1,29 @@
+
+
+source $CONFIG_FILE
+[ "$1" == "-d" ] && CMD=delete || CMD=apply
+
+if [ "$NAMESPACE" == "" ]; then
+  echo "usage: export NAMESPACE=staging; $0"
+  exit 1
+fi
+
+# set max body size and timeout to 2GB and 1200 sec, respectively:
+cat << EOF | kubectl $CMD -f -
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: nginx-config
+  namespace: nginx-ingress
+data:
+  client-max-body-size: 2048m
+  proxy-connect-timeout: 30s
+  proxy-read-timeout: 1200s
+  namespace: nginx-ingress
+EOF
+
+# apply new config by restarting the nginx ingress PODs, if present:
+PODS=$(kubectl get pod -n nginx-ingress | grep '^nginx-ingress' | awk '{print $1}')
+echo $PODS | xargs kubectl delete pod 
+
